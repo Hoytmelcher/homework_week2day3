6b9e12c4da4480d9bba6138d0b7232b55755310e
Hoytmelcher <122131937+Hoytmelcher@users.noreply.github.com>
2023-02-01 14:58:23 -0600

    first commit

---
+++ my_module.py
@@ -0,0 +1,22 @@
+import math
+
+
+
+# square footage of a house
+def square_footage(l,w):
+    dimensions = int(l) * int(w)
+    return dimensions
+
+
+
+
+
+
+
+# circumfrence of a circle
+
+def circumference(r):
+    from math import pi
+    length = 2 * pi * r
+    return length
+
---
+++ test.py
@@ -0,0 +1,7 @@
+from my_module import square_footage
+
+print(square_footage(10,10))
+
+from my_module import circumference
+
+print(circumference(9))
